b4fabfec7d900eea970b1173082718e1091e53c3
Drew Kennedy <drew.kennedy@uwaterloo.ca>
2021-12-08 14:17:27 -0500

    add squares

---
+++ square.cc
@@ -0,0 +1,5 @@
+#include "square.h"
+
+public Square(int colour, int row, int col)
+    : colour { colour }, row { row }, col{ col } {}
+
---
+++ square.h
@@ -0,0 +1,8 @@
+
+struct Square {
+    int colour; // 0 for white, 1 for black
+    int row;
+    char col;
+
+    public Square(int colour, int row, int col); 
+}
